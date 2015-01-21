eea443d36b753210e56bd200413fd2135a488041
Lorenzo Cassulo <lorenzo.cassulo@gmail.com>
2015-01-21 23:44:33 +0100

    Aggiunti esercizi sui metodi

---
+++ 05metodi/rand.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+srand 3124
+puts 'Il metereologo ha detto che c\'è ' + rand(101).to_s + '% possibilità di neve'
+puts 'Il metereologo ha detto che c\'è ' + rand(101).to_s + '% possibilità di pioggia'
+puts 'Il metereologo ha detto che c\'è ' + rand(101).to_s + '% possibilità di sole'
+puts 'Il metereologo ha detto che c\'è ' + rand(101).to_s + '% possibilità di nebbia'
+puts 'Il metereologo ha detto che c\'è ' + rand(101).to_s + '% possibilità di tornado'
+puts 'Il metereologo ha detto che c\'è ' + rand(101).to_s + '% possibilità di eruzioni vulcaniche'
---
+++ 06flussi/maggiore-minore.rb
@@ -0,0 +1,6 @@
+puts 1 > 2
+puts 1 < 2
+puts 1 <= 1
+puts 2 >= 2
+puts 2 == 2
+puts 3 != 6
